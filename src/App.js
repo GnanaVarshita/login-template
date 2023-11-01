911cd1b1da1936fe767d3bd43cae87e6e5fb2863
GnanaVarshita <gnanavarshitagv@gmail.com>
2023-11-02 02:57:54 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState} from "react";
+import FormInput from "./components/FormInput"
 function App() {
+  const[values,setValues]=useState({
+    Username:"",
+    Email:"",
+    PhoneNo:"",
+    Password:"",
+    ConfirmPassword:""
+
+  });
+
+  const inputs=[
+    {
+      id:1,
+      placeholder:"Username",
+      name:"Username",
+      label:"Username",
+      type:"text"
+    },
+    {
+      id:2,
+      placeholder:"Email",
+       name:"Email",
+       label:"Email",
+       type:"text"
+    },
+    {
+      id:3,
+      placeholder:"Phone No." ,
+      name:"PhoneNo",
+      label:"Phone No.",
+      type:"text"
+    },
+    {
+      id:4,
+      placeholder:"Password", 
+      name:"Password",
+      label:"Password",
+      type:"text"
+    },
+    {
+      id:5,
+      placeholder:"ConfirmPassword", 
+      name:"ConfirmPassword",
+      label:"ConfirmPassword",
+      type:"text"
+    }
+  ]
+  const handleSubmit=(e)=>{
+    e.previousDefault();
+
+  }
+
+  const onChange=(e)=>{
+    setValues({...values,[e.target.name]:e.target.value});
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+      {inputs.map((input)=>(
+        <FormInput key={input.id}{...input} 
+        value={values[input.name]} 
+        onChange={onChange}/>
+      ))}
+      <button>Submit</button>
+      </form>
+    
     </div>
   );
 }
